1387bf2498a5499a00d3e77eb2cf4b9ce2ef8bde
affaxerd1 <affaxerdk@gmail.com>
2025-03-09 04:12:48 +0300

    pilot

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className='pattern'/>
+
+      <div className='wrapper'>
+        <header>
+          <img src='./BG.png' alt='hero Banner'/>
+          <h1>Find <span className='text-gradient'>Movies</span> you'll enjoy without a hassle</h1>
+        </header>
+      </div>
+    </main>
+  )
+}
+
+export default App
