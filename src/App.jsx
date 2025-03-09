df894d6782f52d03508203aeb6e058184214318a
affaxerd1 <affaxerdk@gmail.com>
2025-03-09 05:29:08 +0300

    header: banner and search

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from "./components/search.jsx"
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm]= useState('');
   return (
     <main>
       <div className='pattern'/>
 
       <div className='wrapper'>
         <header>
-          <img src='./BG.png' alt='hero Banner'/>
-          <h1>Find <span className='text-gradient'>Movies</span> you'll enjoy without a hassle</h1>
+          <img src='./hero-img.png' alt='hero Banner'/>
+          <h1>Find <span className='text-gradient'>Movies</span> you'll enjoy without the hassle</h1>
         </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        
       </div>
     </main>
   )
