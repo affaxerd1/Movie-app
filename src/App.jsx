5462b24c3658e315b1a2c052fbb604cf34f3c3ac
affaxerd1 <affaxerdk@gmail.com>
2025-03-09 15:18:15 +0300

    setting the environment variable(api key, base url and api option)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Search from "./components/search.jsx"
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method : 'GET',
+  headers : {
+    accept : 'application/json',
+    authorization : `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
 
   const [searchTerm, setSearchTerm]= useState('');
+  useEffect(()=>{
+    
+  },[])
   return (
     <main>
       <div className='pattern'/>
