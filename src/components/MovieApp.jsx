1dc1cb2bcfcb1fb8bacbb9ac02d616eb78887fd6
affaxerd1 <affaxerdk@gmail.com>
2025-03-11 22:42:02 +0300

    presentation logic finish up

--- src/components/MovieApp.jsx
+++ src/components/MovieApp.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MovieApp = ({movie : {title, vote_average, poster_path, release_date} }) => {
+const MovieApp = ({movie : {title, vote_average,original_language,  poster_path, release_date} }) => {
   return (
     <div>
         <div className='movie-card'>
@@ -12,7 +12,14 @@ const MovieApp = ({movie : {title, vote_average, poster_path, release_date} }) =
             <div className='content'>
                 <div className='rating  flex items-center space-x-2'>
                     <img src="Rating.svg" alt="Star Icon" />
+                    <p className='text-white'>	&#8226;</p>
                     <p className='text-white'>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+                    <p className='text-white'>	&#8226;</p>
+                    <p className='lang text-white'>{original_language}</p>
+                    <span className='text-white'>&#8226;</span>
+                    <p className='date text-white'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+
+
 
 {/* stuck at vote average not s */}
                 </div>
