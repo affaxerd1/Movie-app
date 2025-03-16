5eed76b4d833ef9969a17452333871499af5dbcb
affaxerd1 <affaxerdk@gmail.com>
2025-03-16 04:11:22 +0300

    define client and and functionality you want to use in appwrite

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,6 +1,24 @@
+import { Client, Databases } from "appwrite";
+
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
-export const updateSearchCount  =  async() => {
-    console.log(PROJECT_ID,DATABASE_ID,COLLECTION_ID);
+
+const client = new Client().setEndpoint('https://cloud.appwrite.io/v1').setProject(PROJECT_ID);
+const database = new Databases(client);
+
+
+export const updateSearchCount  =  async(searchtERM, MOVIE) => {
+    //1. Use Appwrite SDK to check if searchterm exists in the DB
+    try{
+
+    }
+    catch(error){
+        
+    }
+   
+    // 2. If it does update the count
+    //3. if it doesnt create a new document with searchterm and count
+
+     
 }
